7295aced3d2dc4538658ce5263ff91eb130f87b2
Anton Bykov <a.bykov4@g.nsu.ru>
2025-11-18 17:31:51 +0700

    change docker to simple

--- database/app/config.py
+++ database/app/config.py
@@ -21,8 +21,8 @@ settings = Settings()
 
 def get_db_url():
     return (
-        f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASSWORD}@"
-        f"{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+        f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASSWORD}"
+        f"@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
     )
 
     
