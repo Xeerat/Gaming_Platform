a0ece4281c7373259cc94382458df15d416c6439
Anton Bykov <a.bykov4@g.nsu.ru>
2025-11-27 14:13:20 +0700

    added js and html

--- database/app/main.py
+++ database/app/main.py
@@ -5,7 +5,19 @@ from app.users.router import router as router_users
 from app.friends.router import router as router_friend_req
 from app.maps.router import router as router_maps
 
+
+
 app = FastAPI()
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],           # или твой домен
+    allow_credentials=True,
+    allow_methods=["*"],           # разрешаем OPTIONS, POST, GET и всё остальное
+    allow_headers=["*"],
+)
 
 app.include_router(router_users)
 app.include_router(router_friend_req)
