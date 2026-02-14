635342ad46fc44a1842d9b6b75240dd7f0cc7b61
Xeerat <yuzhaninov_amz@mail.ru>
2026-02-14 15:09:14 +0700

    Optimizate alembic

--- platform/alemb/env.py
+++ platform/alemb/env.py
@@ -1,104 +1,72 @@
 from logging.config import fileConfig
-from sqlalchemy import pool
-from sqlalchemy.ext.asyncio import create_async_engine
-from sqlalchemy.engine import Connection
-from alembic import context
-from alembic.config import Config
-from app.database import DB_URL
-from app.migration.models import Base, User, Temporary
 import asyncio
 
-from pathlib import Path
-from os.path import join
-# Получаем путь до папки с конфигом
-path_to_ini = str(Path(__file__).parent.parent)
+from sqlalchemy.engine import Connection
+from alembic import context
+
+from app.database import DB_URL, async_engine
+from app.migration.models import Base
 
 
-#=========================================================
-# Настройка логирования 
-#=========================================================
-
-# Получаем config-файл
-config = Config(join(path_to_ini, "alembic.ini"))
+# Настройка логирования
+config = context.config
 if config.config_file_name is not None:
-    # Настраиваем логирование в соответствии с параметрами в config
     fileConfig(config.config_file_name)
 
-#=========================================================
-# Метаданные моделей
-#=========================================================
 
-# Данные моделей для изменения структуры таблиц
 target_metadata = Base.metadata
 
-#=========================================================
-# Оффлайн режим
-#=========================================================
 
 def run_offline_migrations() -> None:
-    """ 
-    Функция для генерации SQL-скрипта без дальнейшего выполнения.
-    Подключение к базе данных не происходит.
-    """
-    # Настройка alembic для работы с базой данных
+    """Выполняет оффлайн миграцию базы данных."""
+
     context.configure(
-        # Базы данных разные, поэтому url здесь нужен для определения базы данных текущего проекта
-        # Это не установка соединения с ней
         url=DB_URL,
         target_metadata=target_metadata,
-        # Вставлять ли конкретные значения в sql-запрос или добавлять переменные?
         literal_binds=True
     )
 
-    # Открытие транзакции для безопасной работы с базой данных
     with context.begin_transaction():
-        # Выполнение всех миграций, которые не применены 
         context.run_migrations()
 
-#=========================================================
-# Онлайн режим
-#=========================================================
 
-def do_migrations(connection: Connection) -> None:
+def run_online_migrations(connection: Connection) -> None:
     """
-    Фунция для генерации SQL-скрипта и применения его к базе данных через соединение
-    
-    Входные данные:
-    connection: открытое соединение с базой данных
-    """
-    # Настройка alembic для работы с базой данных
-    # Измения применяются к переданному соединению
-    context.configure(connection=connection, target_metadata=target_metadata)
+    Выполняет онлайн миграцию базы данных.
 
-    # Применение миграций
+    Args:
+        connection: объект соединения с базой данных.
+    """
+
+    context.configure(
+        connection=connection,
+        target_metadata=target_metadata,
+        compare_type=True
+    )
+
     with context.begin_transaction():
         context.run_migrations()
 
 
 async def run_async_migrations() -> None:
-    """ 
-    Функция для асинхронного соединения с базой данных.
-    После соединения применяет миграции.
-    """
-    # Создаем асинхронный движок для соединения с базой данных
-    connectable = create_async_engine(DB_URL, poolclass=pool.NullPool)
-    # Создаем соединение
-    async with connectable.connect() as connection:
-        # Применяем миграции
-        await connection.run_sync(do_migrations)
-    # Выключаем движок
-    await connectable.dispose()
+    """Выполняет онлайн миграцию в асинхронном режиме."""
+
+    async with async_engine.connect() as connection:
+        await connection.run_sync(run_online_migrations)
+
+    await async_engine.dispose()
 
 
-#=========================================================
-# Выбор режима в зависимости от запуска alembic
-#=========================================================
-
-def run_migration():
-    if context.is_offline_mode():
-        run_offline_migrations()
+if context.is_offline_mode():
+    run_offline_migrations()
+else:
+    # Проверка запущенного event loop
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        loop = None 
+    
+    if loop and loop.is_running():
+        asyncio.create_task(run_async_migrations())
     else:
-        # Асинхронный запуск миграций
         asyncio.run(run_async_migrations())
-
-run_migration()
--- platform/app/database.py
+++ platform/app/database.py
@@ -1,15 +1,21 @@
 from os import getenv 
 from dotenv import load_dotenv
 
-#=========================================================
-# Создание URL для связи с базой данных
-#=========================================================
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy import pool
 
-# Загружаем переменные среды из файла .env
+
 load_dotenv()
 
+
 def get_database_url() -> str:
-    """ Функция, которая формирует URL для взаимодействия с базой данных. """
+    """
+    Формирует URL для асинхронного соединения с базой данных.
+     
+    Returns:
+        Строка-url для асинхронного соединения с базой данных.
+    """
+
     user = getenv("DB_USER")
     password = getenv("DB_PASSWORD")
     host = getenv("DB_HOST")
@@ -17,28 +23,36 @@ def get_database_url() -> str:
     name = getenv("DB_NAME")
     return f"postgresql+asyncpg://{user}:{password}@{host}:{port}/{name}"
 
+
 DB_URL = get_database_url()
+async_engine = create_async_engine(DB_URL, poolclass=pool.NullPool)
 
-#=========================================================
-# Получение данных для генерации токена пользователя
-#=========================================================
 
-def get_auth_data():
-    """ Функция возвращающая особые данные для генерации токена """
+def get_token_data() -> dict:
+    """
+    Возвращает особые данные для генерации токена.
+    
+    Returns:
+        Словарь содержащий особые данные для генерации токена.
+    """
+
     return {
         "secret_key": getenv("SECRET_KEY"),
         "algorithm": getenv("ALGORITHM")
     }
 
-#=========================================================
-# Получение данных для верификации через email
-#=========================================================
 
-def get_auth_email():
-    """ Функция возвращающая особые данные для реализации email верификации """
+def get_email_data() -> dict:
+    """
+    Возвращает особые данные для email верификации.
+    
+    Returns:
+        Словарь содержащий особые данные для email верификации.
+    """
+
     return {
         "email_user": getenv("EMAIL_SECRET"),
         "email_password": getenv("EMAIL_SECRET_PASS"),
         "smtp_server": getenv("SMTP_SERVER"),
-        "smtp_port": int(getenv("SMTP_PORT"))
+        "smtp_port": getenv("SMTP_PORT")
     }
